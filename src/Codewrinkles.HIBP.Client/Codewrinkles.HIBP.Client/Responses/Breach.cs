ba6feff9d8d2531274d911f4d31d5c5c6326e7bb
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2023-02-02 07:47:05 +1000

    Breach Respone Model Created

---
+++ src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Responses/Breach.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewrinkles.HIBP.Client.Responses
+{
+    internal class Breach
+    {
+    }
+}
