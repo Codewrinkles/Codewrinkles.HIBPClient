84ce0cbe2040955b6a5b0c4bc977ce8ec814690a
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2023-02-02 07:47:05 +1000

    Breach Respone Model Created

---
+++ src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Responses/Breach.cs
@@ -0,0 +1,21 @@
+﻿namespace Codewrinkles.HIBP.Client.Responses
+{
+    internal class Breach
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Domain { get; set; }
+        public DateTime BreachDate { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int PwnCount { get; set; }
+        public string Description { get; set; }
+        public string[] DataClasses { get; set; }
+        public bool IsVerified { get; set; }
+        public bool IsFabricated { get; set; }
+        public bool IsSensitive { get; set; }
+        public bool IsSpamList { get; set; }
+        public bool IsMalware { get; set; }
+        public string LogoPath { get; set; }
+    }
+}
