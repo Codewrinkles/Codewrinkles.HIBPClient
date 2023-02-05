ed3cb7b553071cb72425cc37f1e0710c2a82b2d3
silvius162003 <silvius162003@yahoo.com>
2023-02-05 22:16:17 +0200

    BadRequestException class

---
+++ src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Exceptions/BadRequestException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewrinkles.HIBP.Client.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public string CustomParameter { get; set; }
+
+        public BadRequestException()
+            : base()
+        {
+        }
+
+        public BadRequestException(string customParameter)
+            : base()
+        {
+            CustomParameter = customParameter;
+        }
+
+        public override string ToString()
+        {
+            return $"Bad Request — the account does not comply with an acceptable format (i.e. it's an empty string). {CustomParameter}";
+        }
+    }
+}
