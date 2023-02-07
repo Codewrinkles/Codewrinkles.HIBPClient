658d22dd6c990351d35fe13db069ee5e171d178e
Bojan Pavlovic <bojanrmv@gmail.com>
2023-02-07 21:06:59 +0200

    - Created HttpClientFactory for creating HttpClient with base api address - internal constructor of HIBPClient for DI uses. - public constructor for non DI uses accepting api key

--- src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Constants.cs
+++ src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Constants.cs
@@ -2,5 +2,13 @@
 
 internal static class Constants
 {
-    
+    public static class Endpoints
+    {
+        const string VERSION = "V3";
+        public const int REQUEST_TIMEOUT = 30;
+        public const string BASE_ENDPOINT = $"https://haveibeenpwned.com/api/{VERSION}";
+
+    }
+
+
 }
---
+++ src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Codewrinkles.HIBP.Client.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewrinkles.HIBP.Client
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddHibpClient(this IServiceCollection services, string apiKey)
+        {
+            var client = HttpClientFactory.CreateDefault(apiKey);
+            services.AddSingleton<IHibpClient>(sp => new HibpClient(client));
+            return services;
+        }
+       
+
+    }
+}
