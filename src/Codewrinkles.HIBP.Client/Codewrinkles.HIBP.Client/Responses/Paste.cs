c70aaf7d429d404a8927c0cdc5e433efc89841b1
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-02-13 20:43:36 +1000

    Ensure all properties match HIBP documentation.

--- src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Responses/Paste.cs
+++ src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Responses/Paste.cs
@@ -5,7 +5,7 @@
         public string Source { get; set; }
         public string Id { get; set; }
         public string? Title { get; set; }
-        public DateTime? PostedDate { get; set; }
+        public DateTime? Date { get; set; }
         public int EmailCount { get; set; }
     }
 }
