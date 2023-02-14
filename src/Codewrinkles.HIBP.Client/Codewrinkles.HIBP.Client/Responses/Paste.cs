f5baf6c41a483a3bbdc69ce9ec433c995713bdc3
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2023-02-14 20:02:39 +1000

    Add constructor to ensure source and id cannot be null.

--- src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Responses/Paste.cs
+++ src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Responses/Paste.cs
@@ -2,10 +2,16 @@
 {
     internal class Paste
     {
-        public string Source { get; set; }
-        public string Id { get; set; }
-        public string? Title { get; set; }
+        public string    Source { get; set; }
+        public string    Id { get; set; }
+        public string?   Title { get; set; }
         public DateTime? Date { get; set; }
-        public int EmailCount { get; set; }
+        public int       EmailCount { get; set; }
+
+        public Paste(string source, string id)
+        {
+            Source = source;
+            Id     = id;
+        }
     }
 }
