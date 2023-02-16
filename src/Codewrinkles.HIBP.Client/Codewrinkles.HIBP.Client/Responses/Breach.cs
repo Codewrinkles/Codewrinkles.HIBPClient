89c5041f1f35ee4a489825932301b9151980a27e
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2023-02-16 06:01:06 -0300

    Updated with constructor and made setters private

--- src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Responses/Breach.cs
+++ src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Responses/Breach.cs
@@ -2,20 +2,39 @@
 {
     internal class Breach
     {
-        public string Name { get; set; }
-        public string Title { get; set; }
-        public string Domain { get; set; }
-        public DateTime BreachDate { get; set; }
-        public DateTime AddedDate { get; set; }
-        public DateTime ModifiedDate { get; set; }
-        public int PwnCount { get; set; }
-        public string Description { get; set; }
-        public string[] DataClasses { get; set; }
-        public bool IsVerified { get; set; }
-        public bool IsFabricated { get; set; }
-        public bool IsSensitive { get; set; }
-        public bool IsSpamList { get; set; }
-        public bool IsMalware { get; set; }
-        public string LogoPath { get; set; }
+        public string Name { get; private set; }
+        public string Title { get; private set; }
+        public string Domain { get; private set; }
+        public DateTime BreachDate { get; private set; }
+        public DateTime AddedDate { get; private set; }
+        public DateTime ModifiedDate { get; private set; }
+        public int PwnCount { get; private set; }
+        public string Description { get; private set; }
+        public string[] DataClasses { get; private set; }
+        public bool IsVerified { get; private set; }
+        public bool IsFabricated { get; private set; }
+        public bool IsSensitive { get; private set; }
+        public bool IsSpamList { get; private set; }
+        public bool IsMalware { get; private set; }
+        public string LogoPath { get; private set; }
+
+        public Breach(string name, string title, string domain, DateTime breachDate, DateTime addedDate, DateTime modifiedDate, int pwnCount, string description, string[] dataClasses, bool isVerified, bool isFabricated, bool isSensitive, bool isSpamList, bool isMalware, string logoPath)
+        {
+            Name = name;
+            Title = title;
+            Domain = domain;
+            BreachDate = breachDate;
+            AddedDate = addedDate;
+            ModifiedDate = modifiedDate;
+            PwnCount = pwnCount;
+            Description = description;
+            DataClasses = dataClasses;
+            IsVerified = isVerified;
+            IsFabricated = isFabricated;
+            IsSensitive = isSensitive;
+            IsSpamList = isSpamList;
+            IsMalware = isMalware;
+            LogoPath = logoPath;
+        }
     }
 }
