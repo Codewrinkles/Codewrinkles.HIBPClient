022917645be07312f244698d16f8ae200bf351d7
Bojan Pavlovic <bojanrmv@gmail.com>
2023-02-26 13:51:12 +0200

    HttpClient Factory with HIBP options

---
+++ src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/Exceptions/ApiKeyException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewrinkles.HIBP.Client.Exceptions
+{
+    public class ApiKeyException : Exception
+    {
+        public ApiKeyException(string message) : base(message)
+        {
+        }
+    }
+}
--- src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/HibpClient.cs
+++ src/Codewrinkles.HIBP.Client/Codewrinkles.HIBP.Client/HibpClient.cs
@@ -5,15 +5,31 @@ namespace Codewrinkles.HIBP.Client;
 public class HibpClient : IHibpClient, IDisposable
 {
     private readonly HttpClient _httpClient;
-
+    /// <summary>
+    /// Internal constructor that taks custom HttpClient
+    /// </summary>
+    /// <param name="httpClient"></param>
     internal HibpClient(HttpClient httpClient)
     {
         _httpClient = httpClient;
     }
+    /// <summary>
+    /// Accepts API key and creates default HttpClient
+    /// </summary>
+    /// <param name="apiKey"></param>
     public HibpClient(string apiKey)
     {
         _httpClient = HttpClientFactory.CreateDefault(apiKey);
     }
+    /// <summary>
+    /// Accepts HibpClientOptions and creates HttpClient with options
+    /// </summary>
+    /// <param name="options"></param>
+    public HibpClient(HibpClientOptions options)
+    {
+        _httpClient = HttpClientFactory.CreateWithOptions(options);
+    }
+
     public void Dispose()
     {
         _httpClient.Dispose();
